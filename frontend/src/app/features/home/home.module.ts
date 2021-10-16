0e705d03f06c3ed67f33c08dee0affe464dcf2c4
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-10-16 20:23:34 -0300

    First commit

---
+++ frontend/src/app/features/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeView } from './views/home/home.view';
+
+@NgModule({
+  declarations: [
+    HomeView
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
